8f6ff7d3d73d50d22c1de6c39fbbd9fe8dba0744
jinghuilin <1322149974@qq.com>
2019-06-15 13:02:54 +0800

    to master

---
+++ source/js/test.js
@@ -0,0 +1,17 @@
+class Person {
+    name = "john";
+    Person(name) {
+        this.name = name;
+    }
+    
+    getPerson() {
+        console(name);
+    }
+}
+
+Person = {
+    name : "john",
+    age : 10,
+}
+
+console(Person.name);
